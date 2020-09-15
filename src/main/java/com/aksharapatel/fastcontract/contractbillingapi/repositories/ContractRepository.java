0a03ae34a03a365788047d25ac4434f9cbaef911
Akshar A. Patel <akshar1095@gmail.com>
2020-09-15 12:22:49 -0400

    Updated contract repository

--- src/main/java/com/aksharapatel/fastcontract/contractbillingapi/repositories/ContractRepository.java
+++ src/main/java/com/aksharapatel/fastcontract/contractbillingapi/repositories/ContractRepository.java
@@ -4,6 +4,9 @@ import com.aksharapatel.fastcontract.contractbillingapi.models.Contract;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ContractRepository extends JpaRepository<Contract, Long> {
+    List<Contract> findContractByContractorId(long contractorId);
 }
